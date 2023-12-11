00869461c8491f499707a32387e4e79fb4bae55f
Dev-Yesung <yeasung67@gmail.com>
2023-12-12 02:14:03 +0900

    project setup
    멀티모듈 프로젝트 설정

---
+++ wave-api/src/main/java/wave/WaveApiApplication.java
@@ -0,0 +1,13 @@
+package wave;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WaveApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WaveApiApplication.class, args);
+	}
+
+}
