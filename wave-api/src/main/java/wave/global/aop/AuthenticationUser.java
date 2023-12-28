4109319d507157abf2c6cb4baa6539704d243a65
Dev-Yesung <yeasung67@gmail.com>
2023-12-28 14:38:54 +0900

    feat: @AuthenticationPrincipal을 Wrapping한 @AuthenticationUser 애노테이션 구현
    - @AuthenticationPrincipal을 @AuthenticationUser을 구현
    - SpEL 식을 활용하여
    annoymousUser와 인증유저를 구분하도록 구현

---
+++ wave-api/src/main/java/wave/global/aop/AuthenticationUser.java
@@ -0,0 +1,14 @@
+package wave.global.aop;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.PARAMETER)
+@AuthenticationPrincipal(expression = "#this == 'annoymousUser' ? null : principal")
+public @interface AuthenticationUser {
+}
